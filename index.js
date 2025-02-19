0ec691b1ba50cb1b39bc376ad7b7b5d3c360d1c2
Mohammad Kaif Shaikh <mohammadkaif6268@gmail.com>
2025-02-19 16:35:40 +0530

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-import app from "./src/app/app.js";
 import ConnectDB from "./src/config/db.js";
 import User from "./src/model/user.model.js"
 import dotenv from "dotenv"
-dotenv.config()
+dotenv.config();
+import express from "express"
+
+const app = express();
+// app.use(cors());
+app.use(express.json());
+
 
 const PORT = process.env.PORT || 5500;
 
@@ -23,6 +28,7 @@ ConnectDB()
 
 app.post("/create" , async(req,res)=>{
    const {name,email,password} = req.body;
+   console.log(name,email,password)
    const user = await User.create({
       name,
       email,
